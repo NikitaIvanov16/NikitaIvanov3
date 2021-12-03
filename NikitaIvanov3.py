33c7d4464be9434f840139db09fcbdfde555ee23
Opilane TTHK <opilane@tthk.ee>
2021-12-03 10:58:27 +0200

    Add project files.

---
+++ NikitaIvanov3.py
@@ -0,0 +1,15 @@
+from module1 import*
+palk=[1200,2500,750,749,1200]
+inimesed=["A","B","C","D","E"]
+def palgad(p,i):
+    valik=input("Средняя зарплата - 1,\nМинимальная зарплата - 2,\nМаксимальная зарплата - 3,\nДобавить человека - 4\n ")
+    if valik=="1":
+        keskmine()
+    elif valik=="2":
+        min()
+    elif valik=="3":
+        maks()
+    elif valik=="4":
+        adding()
+while True:
+    palgad(palk,inimesed)
---
+++ module1.py
@@ -0,0 +1,63 @@
+def adding():
+    #Программа добавляет в списки имя и зарплату
+    #esimene arv nimi: str
+    #teine arv - palk: int 
+    #rtype var: str
+	nimi=input("Введите имя: ")
+	palk=input("Введите зарплату: ")
+	with open("inimesed.txt", "a") as inimesed:
+		inimesed.write(nimi+"\n")	
+	with open("palgad.txt", "a") as palgad:
+		palgad.write(palk+"\n")
+def maks():
+     #Программа проверяет списки и выводит на экран человека с максимальной зарплатой
+    #rtype var: int
+	palgad=[]
+	with open("palgad.txt", "r") as f1:
+		for stro in f1:
+			palgad.append(stro.strip())
+	f=open("inimesed.txt", "r")
+	inimesed=[]
+	for stroka in f:
+		inimesed.append(stroka.strip())
+	f.close
+	zp=palgad.copy()
+	zp.sort()
+	a=zp[0]
+	b=palgad.index(a)
+	print("Самая большая зарплата у "+inimesed[b])
+def min():
+    #Программа проверяет списки и выводит на экран человека с самой маленькой зарплатой
+    #rtype var: int
+	palgad=[]
+	with open("palgad.txt", "r") as f1:
+		for strok in f1:
+			palgad.append(strok.strip())
+	inimesed=[]
+	f=open("inimesed.txt", "r")
+	inimesed=[]
+	for stroka in f:
+		inimesed.append(stroka.strip())
+	zp=palgad.copy()
+	zp.sort()
+	zp.reverse()
+	a=zp[0]
+	b=palgad.index(a)
+	print("Самая маленькая зарплата у "+inimesed[b])
+def keskmine():
+    """
+    Программа проверяет списки и выводит на экран среднюю зарплату
+    rtype var:int
+    """
+    sum=0
+    for palk in p:
+        sum+=palk
+    keskm=sum/len(p)
+    print(keskm)
+    v=0
+    if 0<p.index(keskm)<len(p)-1:
+        kesk=i(p.index(keskm))
+        return keskm
+    else:
+        print("Нет средней зарплаты")
+        return keskm
